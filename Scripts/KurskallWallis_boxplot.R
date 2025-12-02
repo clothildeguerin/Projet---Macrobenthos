b1345be1fcca35d501fd9d457fe407bf7ce8a841
Clothilde GUERIN <clothilde.guerin@gmail.com>
2025-12-02 17:19:07 +0100

    Modifications sur le script Kruskal-Wallis (comparaison des P/B entre les groupes trophiques) --> suppression des lignes de code inutiles et ajout de commentaires

--- Scripts/KurskallWallis_boxplot.R
+++ Scripts/KurskallWallis_boxplot.R
@@ -1,9 +1,11 @@
-boxplot(Prod ~ Groupe, data = KW_BoxPlot,
-        main = "Comparaison de la productivité entre les groupes trophiques, sur toute la période",
+#### Analyse statistique - Comparaison de la productivité (P/B) entre les différents groupes trophiques identifiés, sur l'ensemble de la période étudiée (2010-2022) ####
+
+#### Représentation boxplot et test de Kruskal-Wallis ####
+boxplot(Prod ~ Groupe, data = PBDataFor_KruskallWallis_BoxPlot,
+        main = "Comparaison de la productivité entre les groupes trophiques",
         xlab = "Groupe trophique", ylab = "Productivité (en an-1)")
 
-kruskal.test(Prod ~ Groupe, data = KW_BoxPlot)
+kruskal.test(Prod ~ Groupe, data = PBDataFor_KruskallWallis_BoxPlot)
 
-library(FSA)
-
-dunnTest(Prod ~ Groupe, data = KW_BoxPlot, method = "holm")
+#### Vérification des différences significatives entre les guildes trophiques - Test de Dunn avec correction de Holm ####
+dunnTest(Prod ~ Groupe, data = PBDataFor_KruskallWallis_BoxPlot, method = "holm")
